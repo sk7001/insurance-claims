2d54ed6aa1316bbca1d113c21247ccd335e6b150
Sujith-badvelu <dummy@gmail.com>
2026-03-28 10:36:31 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/insurance_claims_processing_system/entity/Investigator.java
@@ -0,0 +1,27 @@
+
+package com.edutech.insurance_claims_processing_system.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Investigator extends User {
+
+    @OneToMany(mappedBy = "investigator")
+    private List<Investigation> investigations;
+
+    public Investigator() {}
+
+    public List<Investigation> getInvestigations() { return investigations; }
+    public void setInvestigations(List<Investigation> investigations) {
+        this.investigations = investigations;
+    }
+}
+
+
